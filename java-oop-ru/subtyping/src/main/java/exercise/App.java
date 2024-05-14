abc1cabeba27f5c6163a6807d554bc3af8d8155a
@hexlet/cli <support@hexlet.io>
2024-05-14 21:02:15 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -5,16 +5,39 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 // BEGIN
+import java.util.HashMap;
+import java.util.stream.Collectors;
 public class App {
-	public static HashMap<String,String> swapKeyValue(HashMap<String,String> curStorageData) {
+	//public static HashMap<String,String> swapKeyValue(HashMap<String,String> curStorageData) {
+	public static KeyValueStorage swapKeyValue(KeyValueStorage storage) {
+		//var entries = curStorageData.entrySet();
+		//for (var entry : entries) {
+		//	String extrKey = entry.getKey();
+		//	String extrVal = entry.getValue();
+		//	entries.remove(extrKey);
+		//	entries.put(extrVal, extrKey);
+		//}
+		//HashMap<String, String> invCurStorageData = curStorageData.inverse();
+		Map<String, String> curStorageData = storage.toMap();
 		var entries = curStorageData.entrySet();
 		for (var entry : entries) {
-			String extrKey = entry.getKey();
-			String extrVal = entry.getValue();
-			entries.remove(extrKey);
-			entries.put(extrVal, extrKey);
+			storage.unset(entry.getKey());
+			storage.set(entry.getValue(), entry.getKey());
+			
 		}
+		//Map<String, String> mapInversed = 
+		//	curStorageData.entrySet()
+		//		.stream()
+		//		.collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+		//HashMap<String, String> newMap = new HashMap<>();
+		//for (Map.Entry<String, String> change : curStorageData.entrySet()) {
+        //    newMap.put(change.getValue(), change.getKey());
+        //}
+		//KeyValueStorage invStorage = new InMemoryKV(mapInversed);
+		//KeyValueStorage invStorage = new InMemoryKV(newMap);
+		//return invStorage;
 		//HashMap<String, String> invCurStorageData = curStorageData.inverse();
+		return storage;
 	}
 }
 // END
