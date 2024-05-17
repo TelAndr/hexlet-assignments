44bae1bb72994991e5e218209e85cf0703f46250
@hexlet/cli <support@hexlet.io>
2024-05-17 22:07:25 +0300

    submit java-oop-ru/errors

--- java-oop-ru/errors/src/main/java/exercise/App.java
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -4,11 +4,11 @@ package exercise;
 public class App {
 	public static void printSquare(Circle curCircle) {
 		try {
-			System.out.println(curCircle.getSquare()); //transferMoney(inputCard, outputCard, value);
+			System.out.print(Math.round(curCircle.getSquare())); //transferMoney(inputCard, outputCard, value);
 		} catch (NegativeRadiusException e) {
-			System.out.println("Не удалось посчитать площадь");
+			System.out.print("Не удалось посчитать площадь");
 		}
-		System.out.println("Вычисление окончено");
+		System.out.print("\nВычисление окончено");
 	}
 }
 // END
--- java-oop-ru/errors/src/main/java/exercise/Circle.java
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -11,9 +11,9 @@ public class Circle {
 	public int getRadius() {
 		return radius;
 	}
-	public int getSquare() throws NegativeRadiusException{
+	public double getSquare() throws NegativeRadiusException{
 		if (radius >= 0) {
-			return (int)Math.round(Math.PI * radius * radius);
+			return Math.PI * radius * radius;
 		} else {
 			throw new NegativeRadiusException("error code 1", "Input cannot be null or empty"); //throw NegativeRadiusException;
 		}
