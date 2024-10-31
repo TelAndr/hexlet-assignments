1a3444cce0ffe9e982642a391c0a9dde2e9b0f62
@hexlet/cli <support@hexlet.io>
2024-11-01 01:41:49 +0300

    submit java-web-ru/handlers

--- java-web-ru/handlers/src/main/java/exercise/App.java
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -1,24 +1,32 @@
 package exercise;
 
 import io.javalin.Javalin;
+
+import java.io.IOException;
 import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
+
 
 public final class App {
 
-    public static Javalin getApp() {
+    public static Javalin getApp() throws IOException { // Exception
 
         // BEGIN
 		List<String> phones = Data.getPhones();
 		List<String> domains = Data.getDomains();
-        Javalin.create(config -> {
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonArrayPhones = objectMapper.writeValueAsString(phones);
+		String jsonArrayDomains = objectMapper.writeValueAsString(domains);
+        var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        this.get("/phones", ctx -> ctx.result(phones));
-		this.get("/domains", ctx -> ctx.result(domains));
-        this.start(7070);
+        app.get("/phones", ctx -> ctx.result(jsonArrayPhones));
+		app.get("/domains", ctx -> ctx.result(jsonArrayDomains));
+        return app;
         // END
     }
-
+    @NotNull
     public static void main(String[] args) {
         Javalin app = getApp();
         app.start(7070);
