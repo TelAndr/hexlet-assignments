8ea7d446481ed80c1101a0f1cdc6e42d7432ee4f
@hexlet/cli <support@hexlet.io>
2024-11-06 22:32:25 +0300

    submit java-web-ru/handlers

--- java-web-ru/handlers/src/main/java/exercise/App.java
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -4,30 +4,32 @@ import io.javalin.Javalin;
 
 import java.io.IOException;
 import java.util.List;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jetbrains.annotations.NotNull;
 
 
 public final class App {
 
-    public static Javalin getApp() { // throws IOException Exception
+    public static Javalin getApp() throws IOException { //  Exception
 
         // BEGIN
-		List<String> phones = Data.getPhones();
-		List<String> domains = Data.getDomains();
-		ObjectMapper objectMapper = new ObjectMapper();
-		String jsonArrayPhones = objectMapper.writeValueAsString(phones);
-		String jsonArrayDomains = objectMapper.writeValueAsString(domains);
-		var app = Javalin.create(config -> {
-			config.bundledPlugins.enableDevLogging();
-		});
-		app.get("/phones", ctx -> ctx.result(jsonArrayPhones));
-		app.get("/domains", ctx -> ctx.result(jsonArrayDomains));
-		return app;
+        List<String> phones = Data.getPhones();
+        List<String> domains = Data.getDomains();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonArrayPhones = objectMapper.writeValueAsString(phones);
+        String jsonArrayDomains = objectMapper.writeValueAsString(domains);
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/phones", ctx -> ctx.result(jsonArrayPhones));
+        app.get("/domains", ctx -> ctx.result(jsonArrayDomains));
+        return app;
         // END
     }
-    //@NotNull
-    public static void main(String[] args) {
+
+    @NotNull
+    public static void main(String[] args) throws IOException {
         Javalin app = getApp();
         app.start(7070);
     }
