8e1241c2855f6719683acb0470c8dc6a18af4640
@hexlet/cli <support@hexlet.io>
2025-03-22 20:47:52 +0300

    submit java-web-ru/middlewares

--- java-web-ru/middlewares/src/main/java/exercise/App.java
+++ java-web-ru/middlewares/src/main/java/exercise/App.java
@@ -8,21 +8,23 @@ import io.javalin.rendering.template.JavalinJte;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public final class App {
 
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
     public static Javalin getApp() {
-
-        private static String bytesToHex(byte[] hash) {
-		StringBuilder hexString = new StringBuilder(2 * hash.length);
-		for (int i = 0; i < hash.length; i++) {
-			String hex = Integer.toHexString(0xff & hash[i]);
-			if(hex.length() == 1) {
-				hexString.append('0');
-			}
-			hexString.append(hex);
-		}
-		return hexString.toString();
-	}
 		
 		var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
