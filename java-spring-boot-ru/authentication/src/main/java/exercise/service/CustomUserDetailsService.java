09eb6cf304ece2c1b7d7880065c3e0dc81bbd007
@hexlet/cli <support@hexlet.io>
2025-04-17 21:50:46 +0300

    submit java-spring-boot-ru/authentication

---
+++ java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
@@ -0,0 +1,60 @@
+package exercise.service;
+
+import exercise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+
+// BEGIN
+//import io.hexlet.spring.model.User;
+//import io.hexlet.spring.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsManager {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Нужно добавить в репозиторий findByEmail
+        var user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user;
+    }
+
+    @Override
+    public void createUser(UserDetails userData) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'changePassword'");
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'userExists'");
+		return false;
+    }
+}
+// END
